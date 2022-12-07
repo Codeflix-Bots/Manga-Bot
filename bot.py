245893649a509b0eab702346bb78525b2bfd6f03
Daniel Rivero <danielrivgw@gmail.com>
2022-12-07 17:37:05 -0500

    Trying NineManga

--- bot.py
+++ bot.py
@@ -51,6 +51,7 @@ plugin_dicts: Dict[str, Dict[str, MangaClient]] = {
         "MangaBuddy": MangaBuddyClient(),
         "AsuraScans": AsuraScansClient(),
         "NineManga": NineMangaClient(),
+        "MangasIn": MangasInClient(),
     },
     "🇪🇸 ES": {
         "MangaDex": MangaDexClient(language=("es-la", "es")),
