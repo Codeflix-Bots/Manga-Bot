88ac5318f059895f39ba4051f0207345f8b8d85d
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-09-29 19:57:26 +0530

    Update asurascans.py

--- plugins/asurascans.py
+++ plugins/asurascans.py
@@ -1,5 +1,3 @@
-#asurascan.net fixed by r4h4t_69
-
 from typing import List, AsyncIterable
 from urllib.parse import urlparse, urljoin, quote, quote_plus
 
@@ -41,9 +39,7 @@ class AsuraScansClient(MangaClient):
     def chapters_from_page(self, page: bytes, manga: MangaCard = None):
         bs = BeautifulSoup(page, "html.parser")
 
-        container = bs.find("div", {"class": "pl-4 pr-2 pb-4 overflow-y-auto scrollbar-thumb-themecolor scrollbar-track-transparent scrollbar-thin mr-3 max-h-[20rem] space-y-2.5"})
-        
-        li = container.find_all("a", {"class": "block"})
+        li = bs.findAll("a", {"class": "block visited:text-themecolor"})
 
         a = "https://asuracomic.net/series/"
         links = [a + containers.get("href") for containers in li]
@@ -51,7 +47,6 @@ class AsuraScansClient(MangaClient):
         texts = [b + (sub.split('/')[6]) for sub in links]
 
         return list(map(lambda x: MangaChapter(self, x[0], x[1], manga, []), zip(texts, links)))
-
 
     def updates_from_page(self, content):
         bs = BeautifulSoup(content, "html.parser")
